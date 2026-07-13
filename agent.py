2afde6687e8ec82f52cbeaf80b7761da94626737
JuneAr27-meet <junearmaly@gmail.com>
2026-07-13 23:33:41 +0300

    lab 2 submission june

--- agent.py
+++ agent.py
@@ -29,9 +29,25 @@ def run_chat():
 
         reply = response.content[0].text
         print(f'Claude: {reply}')
-        history.append({'role': 'assistant', 'content': reply})
+    
+        in_tokens = response.usage.input_tokens
+        out_tokens = response.usage.output_tokens
+        total_tokens = in_tokens + out_tokens
+        print(f"[Tokens used — In: {in_tokens} | Out: {out_tokens} | Total: {total_tokens}]\n")
+        history.append({'role': 'assistant', 'content': reply})#if deleted the ai will lose context of the conversation
+
 
 run_chat()
-
-
-#its like someone who has amnesia they have to be reminded of the context
+#While deleting  load_dotenv() won't crash your script, the program will crash a few lines later when it tries to use the API key
+#usage.input_tokens: The amount of text you send into the model. 
+#usage.output_tokens: The text the AI generates and sends back to you. 
+#increasing the number of tokens make the response longer and it takes more time to generate.
+#personal reflection1: its like someone who has amnesia they have to be reminded of the context
+# when temp is 0 messages are identical because they arent random at all
+# when temp is 1 messages are phrased differently
+#after 3 parts of conversation theres 6 messages in history because theres the messeges the user sends and the ones the ai returns
+# the ai needs history to understand the context of the conversation 
+# it also needs history to follow instructions given in the system message therefore the ai wont forget the instructions after the first use
+#personal reflection:using mobile data roaming when you are traveling abroad without an unlimited plan, at first you pay a little but it adds up
+#if this :history.append({'role': 'assistant', 'content': reply}) is deleted tokens grow much slower  because you are no longer sending the ai messeges back to the server .
+# deleting the print doesnt affect the ai it only affects the output on the screen.
